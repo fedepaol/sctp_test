ea7ebcb404861e8623821d2fb556de5a1db943a7
Federico Paolinelli <fpaoline@redhat.com>
2019-11-22 16:38:46 +0100

    Add junit reporter for nicely formatted output
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- test/test_suite_test.go
+++ test/test_suite_test.go
@@ -1,13 +1,24 @@
 package test_test
 
 import (
+	"flag"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
 func TestTest(t *testing.T) {
 	RegisterFailHandler(Fail)
+
+	junitPath := flag.String("junit", "junit.xml", "the path for the junit format report")
+	flag.Parse()
+
+	rr := []Reporter{}
+	if junitPath != nil {
+		rr = append(rr, reporters.NewJUnitReporter(*junitPath))
+	}
+	RunSpecsWithDefaultAndCustomReporters(t, "Foo Suite", rr)
 	RunSpecs(t, "Test Suite")
 }
